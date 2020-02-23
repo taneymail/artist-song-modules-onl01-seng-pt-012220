a8d593dc86dbd8e68c774e0119cbee69a38a6dee
T S <taneymail@gmail.com>
2020-02-23 05:47:07 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Song
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
+  
+  attr_accessor :name
+  attr_reader :artist
+
+  @@songs = []
+
+  def self.all
+    @@songs
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+end
